bcd16a4778691c26d91cb017428c27c338e1b6ba
SangJoon Lee <qkqh34k@gmail.com>
2024-05-23 21:24:06 +0900

    init

---
+++ src/test/java/com/boardbuilderslog/bulletin_board/BulletinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardbuilderslog.bulletin_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulletinBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
